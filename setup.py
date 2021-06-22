1c49eb561a805cc1dad9be43593f9e934987415e
Ming <chenmingxiang110@gmail.com>
2021-06-22 16:58:57 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ import setuptools
 
 setuptools.setup(
     name="simplechinese", # 名称
-    version="0.2.1", # 版本
+    version="0.2.7", # 版本
     author="Chen, Mingxiang", # 作者
     author_email="chenmingxiang110@gmail.com", # 邮箱
-    package_data={'': ['*.pickle']},
+    package_data={
+        '': [
+            '**/*.txt',
+            '**/*.pickle',
+            '**/*.npy',
+            'LICENSE',
+        ]},
     description="This package integrates many basic Chinese NLP functions, making Python-based Chinese word processing and information extraction simple and convenient.", # 简介
     long_description="", # 详细介绍
     long_description_content_type="text/markdown", # 详细介绍的文件类型
@@ -19,6 +25,7 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'jieba',
+        'Levenshtein',
         'pandas',
         'sklearn',
         'wordcloud',
