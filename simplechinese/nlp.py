f08f96c6c9e807a9b9c079dd19edf2dc1eafca77
Ming <chenmingxiang110@gmail.com>
2022-07-12 17:43:54 +0800

    Update nlp.py

--- simplechinese/nlp.py
+++ simplechinese/nlp.py
@@ -255,29 +255,3 @@ def find_synonyms(a, n=10):
         res.append((all_words[point], score_cos))
     cache[(a,n)] = res
     return res
-
-def num2chinese(x):
-    num=['零','一','二','三','四','五','六','七','八','九']
-    k=['零','十','百','千','万','十','百']
-
-    def turn(x,y):
-        if y>= 1:
-            a=x//pow(10,y)
-            b=x%pow(10,y)
-            c=num[a]+k[y]
-            if y>4 and b<pow(10,4):
-                c+=k[4]
-            if (len(str(x))-len(str(b))) >= 2 and b != 0:
-                c+=k[0]
-        else:
-            a=x
-            b=0
-            c=num[a]
-        return (c,b,)
-
-    c=turn(x,(len(str(x))-1))
-    a,b=c[0], c[1]
-    while b != 0:
-        a+=turn(b,(len(str(b))-1))[0]
-        b=turn(b,(len(str(b))-1))[1]
-    return a
